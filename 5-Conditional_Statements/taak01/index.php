2787965a8a1f58b3eb85489b3750b2f465a167c0
RienPost <rien@rienpost.nl>
2020-05-18 13:12:44 +0200

    conditional statements taak 1

---
+++ 5-Conditional_Statements/taak01/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$test1 =10;
+$test2 ="gewoon tekst";
+
+echo $test1 == $test2;
+echo $test1 === $test2;
+echo $test1 != $test2;
+echo $test1 > $test2;
+echo $test1 < $test2;
+echo $test1 <> $test2;
+echo $test1 >= $test2;
+echo $test1 <= $test2;
+
+?>
