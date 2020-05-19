6477d933162c2537261a226faf4244c77857d0d1
RienPost <rien@rienpost.nl>
2020-05-19 10:50:00 +0200

    conditional statements taak 2

---
+++ 5-Conditional_Statements/taak02/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$test1 =10;
+$test2 =6;
+
+if($test1 == $test2){
+    echo "ongelijk"
+}
+
+if($test1 == $test2){
+    echo "identiek"
+}
+
+if($test1 == $test2){
+    echo "test1 is groter dan test2"
+}
+
+if($test1 == $test2){
+    echo "test1 is groter of gelijk aan test2"
+}
+
+?>
