6059f09595b27299cbcc78b26fe8ece5dbadb3fc
RienPost <rien@rienpost.nl>
2020-05-20 12:35:34 +0200

    conditions taak 4

---
+++ 5-Conditional_Statements/taak04/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$prod_besteld = 10;
+
+if($prod_besteld < 10) {
+    $prijs = 1.50;
+} elseif($prod_besteld < 20) {
+    $prijs = 1.25;
+} else {
+    $prijs = 1.00;
+}
+
+echo $prijs
+
+?>
